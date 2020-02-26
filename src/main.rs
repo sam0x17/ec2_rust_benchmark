829c33e6c23fa2577f97bb852b1251d9bef01fc2
Sam Johnson <sam@durosoft.com>
2020-02-26 01:09:11 -0500

    remove date from output

--- src/main.rs
+++ src/main.rs
@@ -11,15 +11,11 @@ fn main() {
     // that we can read and write data to.
     let mut stream = listener.accept().unwrap().0;
     let message    = "Hello, World!";
-    let format     = "%a, %d %b %Y %T GMT";
-    let time       = time::now_utc();
     let response   = format!("HTTP/1.1 200 OK\r\n\
-                              Date: {}\r\n\
                               Content-Type: text/html; charset=utf-8\r\n\
                               Content-Length: {}\r\n\
                               \r\n\
                               {}",
-                              time::strftime(format, &time).unwrap(),
                               message.len(),
                               message);
     let _          = stream.write(response.as_bytes());
