dec58485604a4b4583d403825480db0ce3b9ec46
Sam Johnson <sam@durosoft.com>
2020-02-26 03:33:35 -0500

    ignore request errors

--- benchmark.cr
+++ benchmark.cr
@@ -1,14 +1,19 @@
 #!/bin/crystal
 require "http/client"
 
-TOTAL = 1000
+TOTAL = 10000
 TRIGGER_URL = "http://ec2-3-92-234-158.compute-1.amazonaws.com:8080"
 
 puts "starting benchmark..."
 
 started = Time.utc
 TOTAL.times do |i|
-  response = HTTP::Client.get TRIGGER_URL
+  begin
+    response = HTTP::Client.get TRIGGER_URL
+  rescue
+    puts "request error"
+    next
+  end
   raise "non-200 status code" unless response.status_code == 200
   raise "bad response" unless response.body.strip == "{\"message\":\"Go EC2! your request was processed!\"}"
   puts "#{i} / #{TOTAL}"
